0d0478c279f92df22526e6fd28bb92bd701c33d3
nagualchat <31921558+nagualchat@users.noreply.github.com>
2017-09-20 22:02:56 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+token : '',
+mongoConnectUrl : '',
+
+group : -1001083395167,
+channel : -1001135900667,
+
+joinPeriod : 24, // Время, после которого участник считается "старым", в часах
+antispamPeriod : 60 // Период работы антиспама, в секундах
+
+};
+
+module.exports = config;
